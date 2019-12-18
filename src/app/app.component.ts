b45693aea283971811979692cca19d0cdf895e56
Brecht Carlier <erazerbrecht@gmail.com>
2019-12-18 22:50:43 +0100

    Reproduced bug in test application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
-  title = 'AngularIvyNgClassBug';
+
+  public show = false;
+
+  public onClick() {
+    this.show = !this.show;
+  }
 }
